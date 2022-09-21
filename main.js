3f25b32711a5f0dc9e9a2a1c0aa6c9a75c6391b4
Dan W <daniel.winn@student.awesomeinc.org>
2022-09-21 12:30:49 +0000

    this is the working code

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+function car() {
+    let stopped= {
+        moving: false,
+        engineOn: true,
+    }
+    return stopped
+}
+let atLight= car();
+
+atLight.moving= true;
+console.log(atLight);
+
+/*
+functionBrake(){
+return stop
+}
+*/
+
 
